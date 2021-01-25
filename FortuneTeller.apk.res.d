1a416ddd76e1abe59c9c1b55f3d81a1f09256f70
David <77165982+David2do@users.noreply.github.com>
2021-01-25 16:42:45 +0100

    Add files via upload

---
+++ FortuneTeller.apk.res.d
@@ -0,0 +1,18 @@
+/storage/emulated/0/.sketchware/mysc/614/bin/FortuneTeller.apk.res \
+ : /storage/emulated/0/.sketchware/mysc/614/app/src/main/res/layout/main.xml \
+/storage/emulated/0/.sketchware/mysc/614/app/src/main/res/layout/drawer.xml \
+/storage/emulated/0/.sketchware/mysc/614/app/src/main/res/drawable-xhdpi/app_icon.png \
+/storage/emulated/0/.sketchware/mysc/614/app/src/main/res/drawable-xhdpi/default_image.png \
+/storage/emulated/0/.sketchware/mysc/614/app/src/main/res/drawable-xhdpi/good_1.png \
+/storage/emulated/0/.sketchware/mysc/614/app/src/main/res/drawable-xhdpi/good_2.png \
+/storage/emulated/0/.sketchware/mysc/614/app/src/main/res/drawable-xhdpi/good_3.png \
+/storage/emulated/0/.sketchware/mysc/614/app/src/main/res/drawable-xhdpi/good_4.png \
+/storage/emulated/0/.sketchware/mysc/614/app/src/main/res/drawable-xhdpi/good_5.png \
+/storage/emulated/0/.sketchware/mysc/614/app/src/main/res/drawable-xhdpi/good_6.png \
+/storage/emulated/0/.sketchware/mysc/614/app/src/main/res/drawable-xhdpi/good_7.png \
+/storage/emulated/0/.sketchware/mysc/614/app/src/main/res/drawable-xhdpi/good_8.png \
+/storage/emulated/0/.sketchware/mysc/614/app/src/main/res/values/styles.xml \
+/storage/emulated/0/.sketchware/mysc/614/app/src/main/res/values/colors.xml \
+/storage/emulated/0/.sketchware/mysc/614/app/src/main/res/values/strings.xml \
+/storage/emulated/0/.sketchware/mysc/614/app/src/main/assets/fonts \
+/storage/emulated/0/.sketchware/mysc/614/app/src/main/AndroidManifest.xml \
